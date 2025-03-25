a1702bdff90e950e8b039abbe24846a829b5d3bd
mustom09 <mustom@bc.edu>
2025-03-24 23:46:39 -0400

    adjusted forms

--- my_app/forms.py
+++ my_app/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
 class CourseForm(forms.Form):
-    course_number = forms.CharField(max_length=20, label="Course Number")
-    course_name = forms.CharField(max_length=100, label="Course Name")
+    #course_number = forms.CharField(max_length=20, label="Course Number")
+    course_number = forms.CharField(
+        max_length=20, 
+        label="Course Number",
+        widget=forms.TextInput(attrs={'placeholder': 'ex. CSCI3356'})
+    )
+    course_name = forms.CharField(
+        max_length=100,
+        label="Course Name", 
+        widget= forms.TextInput(attrs={'placeholder': 'ex. Software Engineering'})
+    )
