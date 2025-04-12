5dc87f9651de0dfeb415bb3eacc19f82dfc801e2
YuqiWang1106 <wangebb@bc.edu>
2025-04-12 01:43:57 -0400

    New test

--- peer_assessment/settings.py
+++ peer_assessment/settings.py
@@ -178,6 +178,16 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'apikey' 
+# EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY") 
+# DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+
+
 
 CSRF_TRUSTED_ORIGINS = [
     'https://assessmate-app.onrender.com'
